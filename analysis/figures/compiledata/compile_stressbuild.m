50b66af3a4f783970a26ce0c29242a748bc51299
will mcfadden <wmcfadd2@gmail.com>
2016-10-19 23:01:43 -0500

    adding L and mu variation to astress_meas

--- analysis/figures/compiledata/compile_stressbuild.m
+++ analysis/figures/compiledata/compile_stressbuild.m
@@ -59,4 +59,21 @@ for f = files
         end
     end
 end
+
+bp = '/Users/wmcfadden/activ_stress_sweep/';
+cd(bp);
+files = dir;
+files = {files.name};
+
+for f = files
+    if(strfind(f{1},'_scr') )
+        code = strsplit(f{1},'_');
+        if(exist([code{1} '_out.txt'],'file'))
+            code = code{1}
+            measureall
+        end
+    end
+end
+
+
 save('astress_meas','allt','allp','allg','alla','allf','alle','allc','allw','alln')
